445b269705de2c77873d518d9c2f77d17f58a6ed
adriansotoh <adriansotohidalgo16@gmail.com>
2022-09-18 21:34:45 -0500

    Subiendo archivos del proyecto

---
+++ src/main/java/com/walle/WalleApplication.java
@@ -0,0 +1,13 @@
+package com.walle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walle/WalleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
